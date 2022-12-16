c5ba6b9d81c4d56acdd3dfb87ea359f2f1c00f0f
Viren Dhanwani <viren@acidtango.com>
2022-12-16 14:56:25 +0000

    list node implemented

--- src/listNode.ts
+++ src/listNode.ts
@@ -1,3 +1,8 @@
 export class ListNode {
-  constructor(number: number) {}
+  val: number
+  next: ListNode | null
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val
+    this.next = next === undefined ? null : next
+  }
 }
