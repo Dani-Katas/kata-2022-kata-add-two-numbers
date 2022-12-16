770d05cbc31de6c7baf6a9ce9d213f49a35fd1dc
Daniel Ramos <danielramosacosta1@gmail.com>
2022-12-16 14:52:16 +0000

    working test

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,10 +1,21 @@
 import { describe, it, expect } from "vitest"
-import { myFunction } from "./main.js"
+// import { myFunction } from "./main.js"
 
-describe("Default test", () => {
+class ListNode {
+  constructor(number: number) {}
+}
+
+function addNumbers(l1: ListNode, l2: ListNode) {
+  return new ListNode(0)
+}
+
+describe("add two numbers", () => {
   it("should work", () => {
-    const result = myFunction()
+    const l1 = new ListNode(0)
+    const l2 = new ListNode(0)
 
-    expect(result).toBe(true)
+    const result: ListNode = addNumbers(l1, l2)
+
+    expect(result).toEqual(new ListNode(0))
   })
 })
