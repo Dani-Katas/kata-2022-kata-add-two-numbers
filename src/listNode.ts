f7c7fc1aa4bb6afd5f4d6e009d9be7e44734bf12
Daniel Ramos <danielramosacosta1@gmail.com>
2022-12-16 16:00:41 +0000

    finish

--- src/listNode.ts
+++ src/listNode.ts
@@ -1,10 +1,5 @@
 export class ListNode {
-  private readonly value: number
-  private readonly next: ListNode | null
-  constructor(value?: number, next?: ListNode | null) {
-    this.value = value === undefined ? 0 : value
-    this.next = next === undefined ? null : next
-
+  constructor(private readonly value: number, private readonly next?: ListNode) {
     if (this.value >= 10) {
       const valueAdditionString = String(this.value)
       const secondDigitNumber = Number(valueAdditionString[1])
@@ -15,25 +10,20 @@ export class ListNode {
     }
   }
 
-  getNext(): ListNode {
+  private getNext(): ListNode {
     return this.next ?? new ZeroNode()
   }
 
   private hasNext() {
-    return this.next === null
+    return this.next == null
   }
-
-  private safeNext(listToAdd: ListNode): ListNode {
-    return listToAdd.next ?? new ZeroNode()
-  }
-
   add(listToAdd: ListNode): ListNode {
     const additionOfValues = this.value + listToAdd.value
     if (this.hasNext() && listToAdd.hasNext()) {
       return new ListNode(additionOfValues)
     }
 
-    const restOfTheList = this.getNext().add(this.safeNext(listToAdd))
+    const restOfTheList = this.getNext().add(listToAdd.getNext())
 
     return new ListNode(additionOfValues, restOfTheList)
   }
@@ -41,6 +31,6 @@ export class ListNode {
 
 export class ZeroNode extends ListNode {
   constructor() {
-    super(0, null)
+    super(0)
   }
 }
