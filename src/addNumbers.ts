b8f1b741606850e2716bb4b6829cf77784d1ca2f
Daniel Ramos <danielramosacosta1@gmail.com>
2022-12-16 15:03:44 +0000

    refactor ListNode class

--- src/addNumbers.ts
+++ src/addNumbers.ts
@@ -1,8 +1,5 @@
 import { ListNode } from "./listNode.js"
 
 export function addNumbers(firstList: ListNode, secondList: ListNode) {
-  const firstListValue = firstList.val
-  const secondListValue = secondList.val
-
-  return new ListNode(firstListValue + secondListValue)
+  return firstList.add(secondList)
 }
--- src/listNode.ts
+++ src/listNode.ts
@@ -1,8 +1,12 @@
 export class ListNode {
-  val: number
-  next: ListNode | null
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val
+  private readonly value: number
+  private next: ListNode | null
+  constructor(value?: number, next?: ListNode | null) {
+    this.value = value === undefined ? 0 : value
     this.next = next === undefined ? null : next
+  }
+
+  add(listToAdd: ListNode): ListNode {
+    return new ListNode(this.value + listToAdd.value)
   }
 }
